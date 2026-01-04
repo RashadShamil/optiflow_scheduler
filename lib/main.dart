bbb87def35e3fdaa378f5cf7985144d310990184
RashadShamil <SHAMILRASHAD@GMAIL.COM>
2026-01-05 01:18:49 +0530

    Save

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'dart:io'; // To check platform
+import 'package:flutter/material.dart';
+
+// Placeholder screens (Your team will build these later)
+class DesktopEntry extends StatelessWidget {
+  const DesktopEntry({super.key});
+  @override
+  Widget build(BuildContext context) => const Scaffold(body: Center(child: Text("Desktop Dashboard")));
+}
+
+class MobileEntry extends StatelessWidget {
+  const MobileEntry({super.key});
+  @override
+  Widget build(BuildContext context) => const Scaffold(body: Center(child: Text("Mobile Login")));
+}
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'OptiFlow',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      // THE MAGIC SWITCH
+      home: (Platform.isWindows || Platform.isMacOS || Platform.isLinux)
+          ? const DesktopEntry()
+          : const MobileEntry(),
+    );
+  }
+}
